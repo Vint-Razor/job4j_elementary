1094bfd5dc09285672421fe0b9a68c60ae431d88
Vint_Razor <vint-razor@yandex.ru>
2021-08-28 21:25:48 +0500

    7. Расстояние между точками в системе координат [#188]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -9,5 +9,9 @@ public class Point {
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
+        result = Point.distance(0, 0, 2, 2);
+        System.out.println("result (0, 0) to (2, 2) " + result);
+        result = Point.distance(1, 1, 1, 5);
+        System.out.println("result (1, 1) to (1, 5) " + result);
     }
 }

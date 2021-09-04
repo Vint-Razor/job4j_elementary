b4fd30482a34d630ed282c6413560c405962b8ce
Vint_Razor <vint-razor@yandex.ru>
2021-09-04 19:34:33 +0500

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] price = new float[40];
+        String[] names = new String[4];
+        names[0] = "Andrey";
+        names[1] = "Sergey";
+        names[2] = "Danil";
+        names[3] = "Vlad";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + price.length);

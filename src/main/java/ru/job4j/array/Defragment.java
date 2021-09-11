4ad7ef2f81e41493df4ff4a47841661c1aa4ec44
Vint_Razor <vint-razor@yandex.ru>
2021-09-11 14:50:58 +0500

    6.8. Дефрагментация массива [#225 #201586]

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -4,14 +4,11 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                int nullIndex = index; /* указатель на null ячейку. */
-                for (int i = nullIndex + 1; i < array.length; i++) {
+                for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
-                        int notNullIndex = i;
-                        String temp = array[notNullIndex];
-                        array[notNullIndex] = array[nullIndex];
-                        array[nullIndex] = temp;
-                        break; /* переместить первую не null ячейку. Нужен цикл. */
+                        array[index] = array[i];
+                        array[i] = null;
+                        break;
                     }
                 }
 

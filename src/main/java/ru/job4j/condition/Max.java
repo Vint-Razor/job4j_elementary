5a878c6f8cdcb19e8a374f395706b3b90a797138
Vint_Razor <vint-razor@yandex.ru>
2021-09-14 22:36:00 +0500

    3. Перегрузить метод max для трех чисел [#122644 #202725]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,5 +1,22 @@
 package ru.job4j.condition;
 
 public class Max {
+    public static int max(int first, int second) {
+        return first > second ? first : second;
+    }
 
+    public static int max(int first, int second, int third) {
+        return max(
+                first,
+                max(second, third)
+        );
+    }
+
+    public static int max(int first, int second, int third, int four) {
+        return max(
+                first,
+                max(second, third, four)
+        );
+
+    }
 }
